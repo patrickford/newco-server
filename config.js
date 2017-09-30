6e66fb5801c7954c2bf5f63691ad2003d0ff6209
Patrick Ford <patrickford@msn.com>
2017-09-29 20:21:45 -0700

    Added .env and config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.DATABASE_URL = process.env.MONGODB_URI ||
+                      'mongodb://localhost/newco';
+exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
+                      'mongodb://localhost/newco-test');
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'notactualsecret';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 "use strict";
 
-var express = require('express');
-var bodyParser = require('body-parser');
+// Load environment variables
+require('dotenv').config();
 
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+const cors = require('cors';)
+
+const {DATABASE_URL, PORT, JWT_SECRET, JWT_EXPIRY} = require('./config');
+// const {router: usersRouter} = require('./users');
+// const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
+// const {router: videosRouter, Videos} = require('./videos');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
 
 // Set /public directory as static web server
 app.use(express.static('client/public'));
+app.use(morgan('common'));
 app.use(bodyParser.json());
+
+// app.use('/videos', videosRouter);
+// app.use('/users/', usersRouter);
+// app.use('/auth/', authRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
@@ -21,5 +40,51 @@ app.get('/echo/:what', (req, res) => {
   res.status(200).json(resObj);
 })
 
-// Listen for events on port 8080 of localhost
-app.listen(8080, () => console.log('Listening on port 8080'));
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found, you idiot'});
+});
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {runServer, app, closeServer};
