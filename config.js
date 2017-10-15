dc6eb9264eee3ba77af98ebfcf2aa8a07afaf40a
Patrick Ford <patrickford@msn.com>
2017-10-14 22:22:09 -0700

    Added user authorization

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ exports.DATABASE_URL = process.env.MONGODB_URI ||
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
                       'mongodb://localhost/newco-test');
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'notactualsecret';
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
--- index.js
+++ index.js
@@ -11,35 +11,45 @@ const passport = require('passport');
 const cors = require('cors');
 
 const {DATABASE_URL, PORT, JWT_SECRET, JWT_EXPIRY} = require('./config');
-// const {router: usersRouter} = require('./users');
-// const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
+const {router: usersRouter} = require('./users');
+const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
 // const {router: videosRouter, Videos} = require('./videos');
-
-const app = express();
 
 mongoose.Promise = global.Promise;
 
+const app = express();
+
 // Set /public directory as static web server
-//app.use(express.static('client/public'));
+app.use(express.static('/public'));
+
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 // app.use('/videos', videosRouter);
-// app.use('/users/', usersRouter);
-// app.use('/auth/', authRouter);
+app.use('/users/', usersRouter);
+app.use('/auth/', authRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World');
 })
 
-app.get('/echo/:what', (req, res) => {
-  let resObj = {}
-  resObj.host = req.headers.host;
-  resObj.query = req.query;
-  resObj.params = req.params;
-  res.status(200).json(resObj);
-})
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
 
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
 let server;
 
 // this function connects to our database, then starts the server
@@ -77,14 +87,10 @@ function closeServer() {
   });
 }
 
-app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
-});
-
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
 
-module.exports = {runServer, app, closeServer};
+module.exports = {app, runServer, closeServer};
